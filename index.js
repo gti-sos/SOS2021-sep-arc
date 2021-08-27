b642a97a5a211519735e2165a49bfc6099556e6b
anaromero99 <anaromerocaceres@gmail.com>
2021-08-27 12:56:44 +0200

    cors intento 1

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ schizophrenia_stats_api_v2.register(app, BASE_API_PATH_v2);
 var path = require("path");
 
 var cors = require("cors");
+var request = require("request");
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
@@ -22,3 +23,15 @@ app.use(cors());
 app.listen(port, () => {
     console.log(`Server ready listening on ${port}`);
 });
+//INTEGRACIONES
+
+app.use("/proxySchizophrenia", function(req, res) {
+    var apiServerHost = "https://2021-sep-arc.herokuapp.com"
+    
+    console.log(`apiServerHost= <${apiServerHost}>`);
+    console.log(`baseURL = <${req.baseUrl}>`);
+    console.log(`url = <${req.url}>`);
+    var url = apiServerHost + req.url;
+    console.log(`piped: ${req.baseUrl}${req.url} -> ${url}`);
+    req.pipe(request(url)).pipe(res);
+    });
